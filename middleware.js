091ae4acbb891f6985c19e0cb407bf8a8821b80d
ackerman <julio.sergio2709@gmail.com>
2023-02-28 16:33:08 -0500

    Update: middleware fn

--- middleware.js
+++ middleware.js
@@ -1,12 +1,21 @@
+import { get } from '@vercel/edge-config';
+
 export const config = {
-  // Only run the middleware on the admin route
-  matcher: '/admin',
+  matcher: '/test',
 };
 
 export default async function middleware(request) {
   const url = new URL(request.url);
+
+  try {
+    const test = await get('test');
+    console.log('edge-config test succed', test);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
   // You can retrieve IP location or cookies here.
-  if (url.pathname === '/admin') {
+  if (url.pathname === '/test') {
     url.pathname = '/';
   }
   return Response.redirect(url);
