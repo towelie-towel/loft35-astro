725da4e6cbb05320f408e583a9c16af2cdf38f26
ackerman <julio.sergio2709@gmail.com>
2023-02-28 16:44:04 -0500

    Update: Card_Slider to astro component

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,11 +1,9 @@
-import HeaderView from '../components/Header_View';
 import NavBar from '../components/NavBar';
 
 const Header = () => {
   return (
     <header className="mx-auto">
       <NavBar />
-      <HeaderView />
     </header>
   );
 };
