2f378d300eecbf298e60c05851f730b7d2839466
ackerman <julio.sergio2709@gmail.com>
2023-02-28 18:15:50 -0500

    Fix: @astrojs/image is not supported with the @astrojs/vercel/edge adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import image from '@astrojs/image';
