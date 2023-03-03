7277af8a79b56e16a158fe2aaf63857e5b2097c9
ackerman <julio.sergio2709@gmail.com>
2023-03-02 20:50:49 -0500

    Update: Footer to .astro component

--- src/containers/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="flex h-10 items-center justify-center shadow-inner">
-      <p>Copyright © 2023 Loft-35</p>
-    </footer>
-  );
-};
-
-export default Footer;
