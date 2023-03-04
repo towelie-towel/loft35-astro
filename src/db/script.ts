1dea3ef0f5996d3d6aaaa0a0cc2b29606859db29
ackerman <julio.sergio2709@gmail.com>
2023-03-04 09:21:20 -0500

    Update astro components

--- src/db/script.ts
+++ src/db/script.ts
@@ -1,17 +1,36 @@
 import { PrismaClient } from '@prisma/client';
+import data from '@/utils/data';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  // ... you will write your Prisma Client queries here
+async function seedProducts() {
+  for (const product of data.products) {
+    const createdProduct = await prisma.product.create({
+      data: {
+        name: product.name,
+        description: product.description,
+        image: product.image.src,
+        price: parseFloat(product.price),
+        slug: product.slug,
+        stock: product.countInStock,
+
+        category: product.category, //Category @relation(fields: [categoryId], references: [id])
+        categoryId: product.category, //Int
+
+        orders: [], //Order[]  @relation("Product-Orders")
+      },
+    });
+    console.log(`Created product with ID ${createdProduct.id}`);
+  }
 }
 
-main()
-  .then(async () => {
+seedProducts()
+  .catch((error) => console.error(error))
+  .finally(async () => {
     await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
   });
+
+async function seedDatabase() {
+  await seedCategories();
+  await seedProducts();
+}
