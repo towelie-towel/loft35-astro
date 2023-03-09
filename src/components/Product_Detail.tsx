0142747111a2e2594eafe645dd4f1aec8d1ed4fb
ackerman <julio.sergio2709@gmail.com>
2023-03-09 09:22:41 -0500

    Add: Loaders for cards and products

--- src/components/Product_Detail.tsx
+++ src/components/Product_Detail.tsx
@@ -1,5 +1,6 @@
 import type { IProduct } from '../utils/data';
 import React from 'react';
+import ProductLoader from './Product_Loader';
 
 interface IProps {
   product: IProduct | null;
@@ -10,12 +11,7 @@ const ProductDetail: React.FC<IProps> = ({ product, closeProductDetail }) => {
   if (!product) return <></>;
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[var(--secondary-bg-color)] p-4 rounded-lg shadow-lg">
-      <img
-        src={product.image.src}
-        alt={product.image.name}
-        height={250}
-        width={250}
-      />
+      <ProductLoader src={product.image.src} alt={product.image.name} />
       <h1 className="mb-4 text-lg font-medium">{product.name}</h1>
       <button
         onClick={(e) => {
