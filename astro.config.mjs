16fdca21845751523b947786b0109d1edfe61e30
ackerman <julio.sergio2709@gmail.com>
2023-03-12 18:05:13 -0400

    ahh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-//astro.build/config
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
