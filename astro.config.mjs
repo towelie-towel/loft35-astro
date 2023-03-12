9e3604e8f1c2a6603463cffcdffe6eba832ef6a7
ackerman <julio.sergio2709@gmail.com>
2023-03-12 19:15:51 -0400

    Big changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -14,7 +11,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 //astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel({
     analytics: true,
--- src/components/NewFile.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const NewFile = (props: Props) => {
-  return <div>NewFile</div>;
-};
-
-export default NewFile;
