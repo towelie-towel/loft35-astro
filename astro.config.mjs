8a50fabc7d3f82fe454fbdd67c3d443bce12c57e
ackerman <julio.sergio2709@gmail.com>
2023-03-12 13:54:56 -0400

    fixing bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 import react from '@astrojs/react';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-//import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 //astro.build/config
-import node from '@astrojs/node';
+//import node from "@astrojs/node";
 
-// https://astro.build/config
+//astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
+  adapter: vercel({
+    analytics: true,
   }),
 });
