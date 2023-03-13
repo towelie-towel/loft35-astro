be5b541c5875ae9634823e4384508218fa9cdca5
ackerman <julio.sergio2709@gmail.com>
2023-03-12 20:07:58 -0400

    Fix: Load secondary images bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel({
-    analytics: true,
-  }),
+  adapter: vercel(),
 });
